d7e6e3d9d4501f6156e5b7cad0043597c605ce24
bc7608 <bc7608@student.uni-lj.si>
2021-03-21 00:18:28 +0100

    delete odvečnega fajla

--- scrum/scrum_app/views.py
+++ scrum/scrum_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import SubscriberForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
