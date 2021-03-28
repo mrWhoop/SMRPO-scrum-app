3cb4f70abf24bd33c6964b7fe46c6bb1bda7f5ed
bc7608 <bc7608@student.uni-lj.si>
2021-03-28 14:59:14 +0200

    register LastLogin to admin site

--- scrum/scrum_app/admin.py
+++ scrum/scrum_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, Sprint, DevTeamMember, Story, Task
+from .models import Project, Sprint, DevTeamMember, Story, Task, LastLogin
 
 
 # Register your models here.
@@ -8,3 +8,4 @@ admin.site.register(Sprint)
 admin.site.register(DevTeamMember)
 admin.site.register(Story)
 admin.site.register(Task)
+admin.site.register(LastLogin)
